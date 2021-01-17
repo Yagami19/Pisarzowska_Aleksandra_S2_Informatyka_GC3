ce68027fb49a180e1d5088b26219558b4cbf0fa8
Yagami19 <49420305+Yagami19@users.noreply.github.com>
2021-01-17 23:29:45 +0100

    komentarze
    jw

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import sleep
 import datetime
 import csv
 
-
+#Ekran powitania, ktory pokazuje podsumowanie
 def welcomescreen():
     print("Witaj w aplikacji monitorującej twoje wydatki")
     Print_Base_Data()
@@ -14,12 +14,15 @@ def welcomescreen():
 #Funkcje zarzadzajace wydatkami:
 # Menu zarzadzajace wydatkami
 def manage_expenses_menu():
+    #wyczyszenie ekranu
     clear_screen()
+    #Wyswietlenie opcji menu
     print("\nWybierz co chcesz zrobić:")
     print(" [1] Wprowadź wydatek")
     print(" [2] Usun wydatek")
     print(" [3] Wyswietl wydatki")
     print(" [4] Wroc\n")
+    #Wprowadzenie danych przez uzytkownika
     option = input()
     if option == "1":
         insert_expenses()
@@ -29,7 +32,7 @@ def manage_expenses_menu():
         print_expenses()
     elif option == "4":
         main()
-
+    #komunikat o bledzie
     elif option != "":
         print("\n Zly wybor")
         manage_expenses_menu()
@@ -41,33 +44,38 @@ def insert_expenses():
         # input linijki
         Wydatek = input("Wprowadz wydatek")
         #sprawdzic czy wydatek to liczba
-
+        #Sprawdzenie czy uzytkownik chce wprowadzic wydatek z dzisiaj
         ManageDate = input("Czy wydatek jest z dzisiaj? (0 tak)")
         if ManageDate == "0":
             ExpenseDate = datetime.date.today()
             ExpenseDate = ExpenseDate.strftime("%d.%m.%Y")
 
         else:
+            #jezeli nie to uzytkownik wprowadza date
             print("Wprowadz date:")
+            #upewnienie sie czy data jest poprawna
             try:
                 DateEntry = input('Wprowadz date w formacie DD-MM-YYYY')
                 day, month, year = map(int, DateEntry.split('-'))
                 ExpenseDate = datetime.date(year, month, day)
                 ExpenseDate = ExpenseDate.strftime("%d.%m.%Y")
+            #wyswietlenie bledu
             except:
                 print("Bledna data")
                 insert_expenses()
+        #wprowadzenie kategorii wydatkow
         Kategoria = input("Wprowadz kategorie:")
         print(ExpenseDate, Kategoria, Wydatek)
-
+        #komunikat o bledzie
         print("Wydatki wprowadzone pomyslnie")
         sleep(2)
         # Wyswietlenie ponownie menu wydatkow
         manage_expenses_menu()
 
+#funkcja usuwajaca wydatki
 def delete_expenses():
     print("xd")
-
+#funkcja wyswietlajaca wydatki
 def print_expenses():
     print("xd")
 
@@ -107,19 +115,24 @@ def Insert_Category():
     category = 1
     # petla zeby wprowadzac kategorie do pliku
     while category != "0":
-        # input linijki
+        #wprowadzenie kategorii przez uzytkownika
         category = input("Wprowadz kategorie: (0 aby zakonczyc wprowadzanie)\n")
         if category != "0":
+            #otwarcie pliku i zapisanie kategorii do zmiennej
             with open("kategorie.txt", "r") as file:
                 lines = file.readlines()
+                #sprawdzenie czy kategoria istnieje
                 found = any(x == category + "\n" for x in lines)
             with open("kategorie.txt", "a") as file:
+                #jezeli nie to dopisanie kategorii
                 if found != True:
                     file.write(category + "\n")
+                #wyswietlenie komunikatu o bledzie
                 else:
                     print("taka kategoria juz istnieje")
-
+    #wyswietlenie komunikatu o pomyslnym wprowadzeniu kategorii
     print("Kategorie wprowadzone pomyslnie")
+    #odczekanie 2 sekundy,zeby uzytkownik sie zapoznal z komunikatem
     sleep(2)
     # Wyswietlenie ponownie menu kategorii
     Manage_Categories_Menu()
@@ -127,23 +140,30 @@ def Insert_Category():
 
 # Usuwanie kategorii z pliku
 def Delete_Category():
+    #uzytkownik podaje kategorie
     category: str = input("Podaj nazwe kategorii: \n")
+    #otwarcie pliku,
     with open("kategorie.txt", "r") as file:
         lines = file.readlines()
         found = any(x == category+"\n" for x in lines)
+    # sprawdzenie czy kategoria istnieje
     if found==True:
         with open("kategorie.txt", "w") as file:
             for line in lines:
+                #nadpisanie pliku bez podanej kategorii
                 if line.strip("\n") != category:
                     file.write(line)
+        #wyswietlenie komunikatu o poprawnym usunieciu kategorii
         print("kategoria usunieta pomyslnie")
     else:
+        #wyswietlenie komunikatu o bledzie
         print("nie ma takiej kategorii")
+    #odczekanie 2 sekundy,zeby uzytkownik sie zapoznal z komunikatem
     sleep(2)
+    #ponowne wyswietlenie menu kategorii
     Manage_Categories_Menu()
 
 #Wyswietlanie kategorii
-
 def Print_Categories():
     with open("kategorie.txt", "r") as file:
         lines = file.readlines()
